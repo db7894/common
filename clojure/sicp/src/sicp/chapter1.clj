1e13dbd93e0f58f95794be0d209256ce96210f06
bashwork <bashwork@gmail.com>
2012-10-11 15:11:51 -0500

    adding chapter1 of sicp

--- clojure/sicp/src/sicp/chapter1.clj
+++ clojure/sicp/src/sicp/chapter1.clj
@@ -73,3 +73,128 @@
   (loop [x x y y]
     (if (= y 0) x
       (recur y (rem x y)))))
+
+(defn expt
+  "return the exponent of the input"
+  [x y]
+  (loop [t 1 c y]
+    (if (= c 0) t
+      (recur (* t x) (- c 1)))))
+
+(defn fast-expt
+  "return the exponent of the input"
+  [x y]
+  (loop [t 1 c y]
+    (cond
+      (= c 0) t
+      (even? c)(recur (square t) (/ c 2))
+       :else   (recur (* t x) (- c 1)))))
+
+(defn sum
+  "return the sum of the supplied function and range"
+  [term a incr b]
+  (loop [t 0 c a]
+    (if (> c b) t
+      (recur (+ t (term c)) (incr c)))))
+
+(defn sum-of-cubes
+  "return the sum of cubes of the supplied range"
+  [a b]
+  (sum cube a inc b))
+
+(defn sum-of-integers
+  "return the sum of integers of the supplied range"
+  [a b]
+  (sum identity a inc b))
+
+(defn pi-sum
+  "return an approximation of pi"
+  [a b]
+  (letfn [(term [x] (/ 1.0 (* x (+ x 2)))) 
+          (incr [x] (+ x 4))] 
+  (sum term a incr b)))
+
+(defn integral
+  "return the integral of a function in the supplied range"
+  [f a b dx]
+  (let [init (+ a (/ dx 2.0))
+        incr (fn [x] (+ dx x))]
+    (* dx (sum f init incr b))))
+
+(defn accumulate
+  "return an accumulation defined by the variables"
+  [combine zero term a incr b]
+  (loop [t zero c a]
+    (if (> c b) t
+      (recur (combine t (term c)) (incr c)))))
+
+(defn sum-of-evens
+  "return the sum of integers of the supplied range"
+  [a b]
+  (accumulate + 0 identity a #(+ 2 %) b))
+
+(defn average-damp
+  "return the supplied function with average damping"
+  [f]
+  (fn [x] (average x (f x))))
+
+(defn positive?
+  "return if the supplied value is positive"
+  [x]
+  (>= x 0))
+
+(defn negative?
+  "return if the supplied value is negative"
+  [x]
+  (< x 0))
+
+(defn search-for-midpoint
+  "using the function return the midpoint of the supplied values"
+  [f neg pos]
+  (let [mid    (average neg pos)
+        close? (fn [x y] (< (abs (- x y)) 0.001))
+        value  (f mid)]
+    (if (close? neg pos) mid
+      (cond
+        (positive? value) (search-for-midpoint f neg mid)
+        (negative? value) (search-for-midpoint f mid pos)
+         :else mid))))
+
+(defn half-interval
+  "a helper function for using the midpoint search"
+  [f a b]
+  (let [aval (f a)
+        bval (f b)]
+    (cond
+      (and (negative? aval)(positive? bval))
+        (search-for-midpoint f a b)
+      (and (negative? bval)(positive? aval))
+        (search-for-midpoint f b a)
+       :else (throw (Exception. "values are not of opposite sign")))))
+
+(defn fixed-point
+  "return the fixed point for the given function"
+  [f initial]
+  (letfn [
+    (close? [x y] (< (abs (- x y)) 0.001))
+    (dotry  [guess]
+      (let [after (f guess)]
+        (if (close? guess after) after
+          (dotry after))))]
+    (dotry initial)))
+
+(defn deriv
+  "return the derivative of the supplied function"
+  [f]
+  (let [dx 0.00001]
+    (fn [x] (/ (- (f (+ x dx)) (f x)) dx)))) 
+
+(defn newton-transform
+  "transform a function for newtons method"
+  [f]
+  (fn [x] (- x (/ (f x) ((deriv f) x)))))
+
+(defn newton-method
+  "uses newtons method to find a fixed point"
+  [f guess]
+  (fixed-point (newton-transform f) guess))
--- clojure/sicp/test/sicp/chapter1_test.clj
+++ clojure/sicp/test/sicp/chapter1_test.clj
@@ -75,4 +75,58 @@
 (deftest gcd-test
   (testing "that gcd works correctly"
     (is (= 10 (int (gcd 20 50))))
-    (is (= 8 (int (gcd 16 8))))))
+    (is (=  8 (int (gcd 16 8))))))
+
+(deftest expt-test
+  (testing "that expt works correctly"
+    (is (=  8 (expt 2 3)))
+    (is (= 27 (expt 3 3)))))
+
+(deftest fast-expt-test
+  (testing "that fast-expt works correctly"
+    (is (=  8 (fast-expt 2 3)))
+    (is (= 27 (fast-expt 3 3)))))
+
+(deftest sum-of-cubes-test
+  (testing "that sum-of-cubes works correctly"
+    (is (=  36 (sum-of-cubes 1 3)))
+    (is (= 432 (sum-of-cubes 3 6)))))
+
+(deftest sum-of-integers-test
+  (testing "that sum-of-integers works correctly"
+    (is (=   55 (sum-of-integers 0 10)))
+    (is (= 5050 (sum-of-integers 0 100)))))
+
+(deftest pi-sum-test
+  (testing "that pi-sum works correctly"
+    (is (= 313 (int (* 800 (pi-sum 1 1000)))))))
+
+(deftest sum-of-evens-test
+  (testing "that sum-of-evens works correctly"
+    (is (=   30 (sum-of-evens 0 10)))
+    (is (= 2550 (sum-of-evens 0 100)))))
+
+(deftest integral-test
+  (testing "that integral works correctly"
+    (is (= 24 (int (* 100 (integral cube 0 1 0.01)))))))
+
+(deftest half-interval-test
+  (testing "that half-interval works correctly"
+    (letfn [(method [x] (- (cube x) (* 2 x) 3))]
+      (is (= 189 (int (* 100 (half-interval method 1.0 2.0))))))))
+    ;;(is (= 314 (int (* 100 (half-interval sin 2.0 4.0)))))))
+
+(deftest fixed-point-test
+  (testing "that fixed-point works correctly"
+    (let [method (fn [x] (/ 2 x))
+          damped (average-damp method)]
+      (is (= 141 (int (* 100 (fixed-point damped 1.0))))))))
+
+(deftest deriv-test
+  (testing "that derive works correctly"
+    (is (= 75 (int ((deriv cube) 5))))))
+
+(deftest newton-method-test
+  (testing "that newton-method works correctly"
+    (let [method (fn [x] (- (* x x) 2))]
+      (is (= 141 (int (* 100 (newton-method method 1.0))))))))
