98514a3f8b0296612f80f3f417e11f315955cdb5
Galen Collins <gccollin@amazon.com>
2013-04-23 23:40:51 +0000

    Adding more trees

--- python/algorithms/tree/kdtree.py
+++ python/algorithms/tree/kdtree.py
@@ -1,9 +1,16 @@
-from tree import BinaryNode
+from binary import BinaryNode
 
 class KDNode(BinaryNode):
 
     def __init__(self, value, axis=0, left=None, right=None):
-        super(KDTree, self).__init__(value, left, right)
+        ''' Initializes a new instance of the kdtree
+
+        :param value: The n-dimensional point value of this node
+        :param axis: The axis this node uses
+        :param left: The left child of this node (default None)
+        :param right: The right child of this node (default None)
+        '''
+        super(KDNode, self).__init__(value, left, right)
         self.k = len(value)
         self.x = axis
 
@@ -15,12 +22,12 @@ class KDNode(BinaryNode):
         :param depth: The current depth of the tree (default 0)
         :returns: The initialized kd-tree
         '''
-        if not xs: return None      # recursion base case
+        if not ys: return None      # recursion base case
         k = len(ys[0])              # find the number of axis
         x = depth % k               # choose the correct axis
-        ys.sort(key=lambda y: y[x]  # sort by current axis
-        m = len(xs) // 2            # select mid point
-        node = klass(xs[m], axis=x) # create mid point node
+        ys.sort(key=lambda y: y[x]) # sort by current axis
+        m = len(ys) // 2            # select mid point
+        node = klass(ys[m], axis=x) # create mid point node
         node.left  = klass.create(ys[:m], depth + 1)
         node.right = klass.create(ys[m+1:], depth + 1)
         return node
@@ -30,19 +37,49 @@ class KDNode(BinaryNode):
 
         :param point: The point to insert in the tree
         '''
-        curr, node = self, type(self)(point)
+        curr = self
         while curr:
-            x = (curr.x + 1) % curr.k
-            if curr.value[curr.x] > value[curr.x]:
+            print curr.value, point
+            xn = (curr.x + 1) % curr.k
+            if curr.value == point: break
+            if curr.value[curr.x] > point[curr.x]:
                 if not curr.left:
-                    curr.left = type(self)(point, axis=x)
+                    curr.left = type(self)(point, axis=xn)
                     break
                 else: curr = curr.left
-            elif curr.value[curr.x] < value[curr.x]:
+            elif curr.value[curr.x] < point[curr.x]:
                 if not curr.right:
-                    curr.right = type(self)(point, axis=x)
+                    curr.right = type(self)(point, axis=xn)
                     break
                 else: curr = curr.right
+
+    def neighbors(self, point, k):
+        ''' Given a point and a number k, return
+        the k nearest neighbors of the given point.
+
+        :param point: The point to find the neighbors of
+        :param k: The number of neighbors to return
+        :returns: The k-nearest neighbors
+        '''
+        pass
+
+    def __in_region(self, region, bound):
+        '''
+        '''
+        k = self.k // 2
+        return (all(r <= b for r, b in zip(region[:k], bound[:k])) 
+            and all(r >= b for r, b in zip(region[k+1:], bound[k+1:])))
+
+    def search(self, region):
+        ''' Given a point and a number k, return
+        the k nearest neighbors of the given point.
+
+        :param point: The point to find the neighbors of
+        :param k: The number of neighbors to return
+        :returns: The k-nearest neighbors
+        '''
+        pass
+
 
 #------------------------------------------------------------
 # Implement points in leaf only tree
---
+++ python/algorithms/tree/test_kdtree.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import unittest
+from kdtree import KDNode
+from binary import dfs_inorder_iter
+
+#---------------------------------------------------------------------------#
+# helper methods
+#---------------------------------------------------------------------------#
+def is_valid(tree):
+    ''' Given a kdtree, validate that it is valid
+
+    :param tree: The tree to validate for correctness
+    :returns: True if valid, False otherwise
+    '''
+    if not tree: return True                            # if we are at a leaf, we are valid
+    l,c,r = tree.left, tree.value[tree.x], tree.right   # unpack values
+    if l and l.value[tree.x] > c: return False          # left child should not be greater than current 
+    if r and r.value[tree.x] < c: return False          # right child should not be greater than current 
+    return is_valid(tree.left) and is_valid(tree.right) # all children should be valid
+
+
+#---------------------------------------------------------------------------#
+# fixture
+#---------------------------------------------------------------------------#
+class KDNodeTest(unittest.TestCase):
+    ''' Code to validate that the kdtree implementation is correct.
+    '''
+
+    def setUp(self):
+        self.size   = 5
+        self.points = [(x, y) for x in range(self.size) for y in range(self.size)]
+        self.tree   = KDNode.create(self.points)
+
+    def test_create(self):
+        ''' Test that the kdtree is created correctly '''
+        self.assertTrue(is_valid(self.tree))
+
+    def test_add_to_tree(self):
+        ''' Test that we can correctly add points to the kdtree '''
+        for point in range(self.size + 1):
+            self.tree.add((self.size + 1, point))
+            self.tree.add((point, self.size + 1))
+        self.assertTrue(is_valid(self.tree))
+
+
+#---------------------------------------------------------------------------#
+# main
+#---------------------------------------------------------------------------#
+if __name__ == "__main__":
+    unittest.main()
